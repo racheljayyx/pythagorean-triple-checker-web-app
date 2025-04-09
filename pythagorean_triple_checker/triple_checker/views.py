6e7a555c83eee1edb23a9739a8e22547988b0b9b
Rachel Jaisey <jaiseyrachel@gmail.com>
2025-04-09 17:07:09 +0100

    feat: update view to render 'index.html' template instead of returning static message

--- pythagorean_triple_checker/triple_checker/views.py
+++ pythagorean_triple_checker/triple_checker/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 
 def triple_checker(request):
-    return HttpResponse("Welcome to the Pythagorean Triple Checker!")
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
